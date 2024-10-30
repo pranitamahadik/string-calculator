292539d2a5367aa35c611eb6d1cb47af01e19211
pranitamahadik <pranitamahadik73@gmail.com>
2024-10-30 15:40:51 +0530

    Added test case for empty string

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+require './string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    it 'should return 0 when empty string' do
+      expect(calculator.add("")).to eq(0)
+    end
+  end 
+end
