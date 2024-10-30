0ebfb01d0b658168ad3a06946d84296ed6a51dae
pranitamahadik <pranitamahadik73@gmail.com>
2024-10-30 16:24:29 +0530

    Added test cases for a single number and for two comma-separated numbers.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe StringCalculator do
     it 'should return 0 when empty string' do
       expect(calculator.add("")).to eq(0)
     end
+
+    it 'should return number itself when it is single number' do
+      expect(calculator.add("1")).to eq(1)
+    end
+
+    it 'should return sum of two comma-seprated number' do
+      expect(calculator.add("1,5")).to eq(6)
+    end
   end 
 end
