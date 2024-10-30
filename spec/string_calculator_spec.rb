282eb94024e5f81d4b65ece78d73b30e8e7eb2d4
pranitamahadik <pranitamahadik73@gmail.com>
2024-10-30 16:54:47 +0530

    Added test cases for the sum of multiple numbers and for the sum of numbers with new line delimiters.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,8 +12,16 @@ RSpec.describe StringCalculator do
       expect(calculator.add("1")).to eq(1)
     end
 
-    it 'should return sum of two comma-seprated number' do
+    it 'should return sum of two comma-seprated numbers' do
       expect(calculator.add("1,5")).to eq(6)
+    end
+
+    it 'should return sum of multiple comma-seprated numbers' do
+      expect(calculator.add("1,2,3,4")).to eq(10)
+    end
+
+    it 'should return sum of numbers with new lines as delimiters' do
+      expect(calculator.add("1\n2,3")).to eq(6)
     end
   end 
 end
