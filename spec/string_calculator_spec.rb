1aea17405a5e73eb11c5c9c61eea3cfb8f2f13cf
pranitamahadik <pranitamahadik73@gmail.com>
2024-10-30 17:08:31 +0530

    Added test case for supports different delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ RSpec.describe StringCalculator do
     it 'should return sum of numbers with new lines as delimiters' do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports different delimiters' do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end 
 end
