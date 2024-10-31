08b9d2a59ea6fe467a20519e61411b7a4e98b6dc
pranitamahadik <pranitamahadik73@gmail.com>
2024-10-31 11:24:27 +0530

    Added code in the add method to raise an exception for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,3 +1,5 @@
+class NegativeNumberError < StandardError; end
+
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
@@ -7,6 +9,9 @@ class StringCalculator
     #here input strings replace all delimiter with , and then convert into array using split.
     number_array = numbers.gsub(delimiters_list, ",").split(",").map(&:to_i)
 
+    negative_numbers = number_array.select{|n| n < 0}
+    raise NegativeNumberError, "Negative numbers not allowed #{negative_numbers.join(', ')}" unless negative_numbers.empty?
+    
     number_array.sum
   end
 end
